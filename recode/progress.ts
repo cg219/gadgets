7358c6b2b820ced6108b4c0c38c1f14996947373
Mente <dev@imkreative.com>
2023-11-19 12:23:11 -0500

    Fix Progressbar using ffmpeg 6+ Update Dax version

--- recode/progress.ts
+++ recode/progress.ts
@@ -2,7 +2,7 @@ function parseProgress(progress: string, total: number, start: number) {
     const arr = progress.split(/\r?\n/);
     const micros = arr.find((l) => l.includes('out_time_ms'))?.split('=').at(1);
 
-    return micros ? ((Number(micros) / 1000000) + start) / total : 0;
+    return micros ? ((Number(Math.abs(micros)) / 1000000) + start) / total : 0;
 }
 
 function getProgress(stream: ReadableStream, total = 0, start = 0) {
