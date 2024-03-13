3fb6ae56869cc5a885658459dc41ba186e2dde22
Mente <>
2024-03-13 01:38:50 -0400

    update imports

--- recode/vendor/deno.land/x/dax@0.35.0/src/commands/echo.ts
+++
@@ -1,7 +0,0 @@
-import { CommandContext } from "../command_handler.ts";
-import { ExecuteResult, resultFromCode } from "../result.ts";
-
-export function echoCommand(context: CommandContext): ExecuteResult {
-  context.stdout.writeLine(context.args.join(" "));
-  return resultFromCode(0);
-}
